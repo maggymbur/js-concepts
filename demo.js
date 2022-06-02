d83c27be7da9f75c6cf26cd58d5df94d69ef66e0
maggymbur <mburumaggy62@gmail.com>
2022-06-02 16:50:21 +0300

    first commit

---
+++ demo.js
@@ -0,0 +1,9 @@
+let cars = ["bmw", "audi", "limo"];
+ document.getElementById("demo").innerHTML=cars;
+ let bmw = cars[0];
+ document.getElementById("demo").innerHTML=bmw;
+ let len = cars.length;
+ document.getElementById("demo").innerHTML=len;
+ let last = cars[cars.length-1];
+ document.getElementById("demo").innerHTML=last;
+
